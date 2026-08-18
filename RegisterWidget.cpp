c406d2f3346869e91580e028188ce9587a961094
miraeKim <mirae.kim@gytni.com>
2026-08-18 09:32:11 +0900

    fix: 기록 배경 색상 변경
    - #eff5f4 -> #f3f3f3

--- RegisterWidget.cpp
+++ RegisterWidget.cpp
@@ -12,7 +12,7 @@ RegisterWidget::RegisterWidget(QWidget *parent, const QString& sTextFirst, const
 								"background-color: #eaeaea;"
 								"}" 
 								"QFrame {"
-								"background-color: #eff5f4;"
+								"background-color: #f3f3f3;"
 								"color: #191919;"
 								"}" );
 
